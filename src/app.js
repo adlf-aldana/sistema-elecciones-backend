2c7565528392f046a3c8553a8846ff827782b4fd
Adolfo <adlf.aldana@gmail.com>
2021-06-30 10:32:52 -0400

    habilitando rutas

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/../public")));
 
 // // routes
-// app.use("/api/lista_estudiantes", require("./routes/universitarios"));
-// app.use("/api/frente_universitario", require("./routes/frente_universitario"));
-// app.use(
-//   "/api/consulta_universitario_cu",
-//   require("./routes/consulta_universitario")
-// );
-// app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/votante", require("./routes/votante"));
-// app.use("/api/consultaVotante", require("./routes/consultasVotante"));
+app.use("/api/lista_estudiantes", require("./routes/universitarios"));
+app.use("/api/frente_universitario", require("./routes/frente_universitario"));
+app.use(
+  "/api/consulta_universitario_cu",
+  require("./routes/consulta_universitario")
+);
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/votante", require("./routes/votante"));
+app.use("/api/consultaVotante", require("./routes/consultasVotante"));
 
 module.exports = app;
