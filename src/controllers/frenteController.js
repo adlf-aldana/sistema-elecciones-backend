40badf3787b842eb6d6583ac581630efd1b501ed
Adolfo <adlf.aldana@gmail.com>
2021-07-19 22:19:06 -0400

    ahora se puede editar a los frentes

--- src/controllers/frenteController.js
+++ src/controllers/frenteController.js
@@ -26,49 +26,34 @@ frenteCtrl.createFrente = async (req, res) => {
     const universitarios = await universitarioModel.find();
 
     let universitario = [];
-    // if (req.body.cuEncargado.length > 1) {
-    //   req.body.cuEncargado.map((cu) => {
-    //     console.log("entro");
-    //     universitario.push(
-    //       universitarios.filter(
-    //         (res) =>
-    //           crypto.AES.decrypt(res.cu, "palabraClave").toString(
-    //             crypto.enc.Utf8
-    //           ) === cu
-    //       )
-    //     );
-    //   });
-    // } else {
-    //   universitario.push(
-    //     universitarios.filter(
-    //       (res) =>
-    //         crypto.AES.decrypt(res.cu, "palabraClave").toString(
-    //           crypto.enc.Utf8
-    //         ) === cu
-    //     )
-    //   );
-    // }
+    if (req.body.cuEncargado.length > 1) {
+      req.body.cuEncargado.map((cu) => {
+        universitario.push(
+          universitarios.filter(
+            (res) =>
+              crypto.AES.decrypt(res.cu, "palabraClave").toString(
+                crypto.enc.Utf8
+              ) === cu
+          )
+        );
+      });
+    } else {
+      universitario.push(
+        universitarios.filter(
+          (res) =>
+            crypto.AES.decrypt(res.cu, "palabraClave").toString(
+              crypto.enc.Utf8
+            ) === cu
+        )
+      );
+    }
 
-    // VERIFICANDO QUE LOS CU DE LOS UNIVERSITARIOS EXISTAN, DENTRO DE LOS UNIVERSITARIOS EXISTENTES
-    universitario.map(async (u) => {
-      if (u.length < 1) {
+    for (let i = 0; i < universitario.length; i++) {
+      if (universitario[i].length < 1) {
         await unlink(path.resolve("./public/images/" + req.file.filename));
         return res.status(400).json({ msg: "Error: Universitario no existe " });
       }
-    });
-    //   let frentes = await frenteModel.find();
-    //   let encargado = await frentes.filter(
-    //     (res) =>
-    //       crypto.AES.decrypt(res.cuEncargado, "palabraClave").toString(
-    //         crypto.enc.Utf8
-    //       ) === cuEncargado
-    //   );
-    //   if (encargado.length > 0) {
-    //     await unlink(path.resolve("./public/images/" + req.file.filename));
-    //     return res.status(400).json({
-    //       msg: "Error: Ya existe un Frente con el Carnet Universitario",
-    //     });
-    //   }
+    }
 
     for (let i = 0; i < req.body.cargo.length; i++) {
       const newFrente = new frenteModel({
@@ -76,15 +61,6 @@ frenteCtrl.createFrente = async (req, res) => {
         registro: req.body.registro[0],
         logoFrente: "/images/" + req.file.filename,
         cargo: req.body.cargo[i],
-
-        // cuEncargado: req.body.cuEncargado[i],
-        // celularEncargado: req.body.celularEncargado[i],
-
-        // nombreFrente: crypto.AES.encrypt(
-        //   req.body.nombreFrente,
-        //   "palabraClave"
-        // ).toString(),
-        // cargo: crypto.AES.encrypt(req.body.cargo[i], "palabraClave").toString(),
         cuEncargado: crypto.AES.encrypt(
           req.body.cuEncargado[i],
           "palabraClave"
@@ -93,18 +69,9 @@ frenteCtrl.createFrente = async (req, res) => {
           req.body.celularEncargado[i],
           "palabraClave"
         ).toString(),
-        // logoFrente: "/images/" + req.file.filename,
       });
       await newFrente.save();
     }
-
-    // await newFrente.save();
-    //   const newFrente = new frenteModel({
-    //     nombreFrente: req.body.nombreFrente,
-    //     cuEncargado: req.body.cuEncargado,
-    //     celularEncargado: req.body.celularEncargado,
-    //     logoFrente: "/images/" + req.file.filename,
-    //   });
     res.send({ msg: "Guardado" });
   } catch (error) {
     console.log(error);
@@ -139,21 +106,72 @@ frenteCtrl.getFrente = async (req, res) => {
 };
 
 frenteCtrl.updateFrente = async (req, res) => {
-  if (req.file) {
-    // ELIMINANDO IMAGEN PASADA
-    const { logoFrente } = await frenteModel.findById(req.params.id);
-    await unlink(path.resolve("./public/" + logoFrente));
+  try {
+    // const archivo = req.file;
+    // if (archivo) {
+    //   // ELIMINANDO IMAGEN PASADA
+    //   let { logoFrente } = await frenteModel.findById(req.params.id);
+    //   await unlink(path.resolve("./public/" + logoFrente));
+    //   // }
+    // }
+
+    const universitarios = await universitarioModel.find();
+
+    let universitario = [];
+    if (req.body.cuEncargado.length > 1) {
+      req.body.cuEncargado.map((cu) => {
+        universitario.push(
+          universitarios.filter(
+            (res) =>
+              crypto.AES.decrypt(res.cu, "palabraClave").toString(
+                crypto.enc.Utf8
+              ) === cu
+          )
+        );
+      });
+    } else {
+      universitario.push(
+        universitarios.filter(
+          (res) =>
+            crypto.AES.decrypt(res.cu, "palabraClave").toString(
+              crypto.enc.Utf8
+            ) === cu
+        )
+      );
+    }
+
+    for (let i = 0; i < universitario.length; i++) {
+      if (universitario[i].length < 1) {
+        if (req.file)
+          await unlink(path.resolve("./public/images/" + req.file.filename));
+        console.log(universitario[i].length);
+        return res.status(400).json({ msg: "Error: Universitario no existe " });
+      }
+    }
+
+    for (let i = 0; i < req.body.cargo.length; i++) {
+      await frenteModel.findByIdAndUpdate(req.body.id[i], {
+        nombreFrente: req.body.nombreFrente,
+        registro: req.body.registro[0],
+        logoFrente: req.file
+          ? "/images/" + req.file.filename
+          : req.body.logoFrente,
+        cargo: req.body.cargo[i],
+        cuEncargado: crypto.AES.encrypt(
+          req.body.cuEncargado[i],
+          "palabraClave"
+        ).toString(),
+        celularEncargado: crypto.AES.encrypt(
+          req.body.celularEncargado[i],
+          "palabraClave"
+        ).toString(),
+      });
+    }
+    res.json({ msg: "guardado correctamente" });
+  } catch (e) {
+    console.log(e);
+    res.json({ msg: "Error: Se produjo un error al actualizar los datos" });
   }
-  const { nombreFrente, cuEncargado, celularEncargado, logoFrente, cantVotos } =
-    req.body;
-  const frente = await frenteModel.findByIdAndUpdate(req.params.id, {
-    nombreFrente,
-    cuEncargado,
-    celularEncargado,
-    logoFrente: req.file ? "/images/" + req.file.filename : logoFrente,
-    cantVotos,
-  });
-  res.json({ frente });
 };
 
 frenteCtrl.deleteFrente = async (req, res) => {
